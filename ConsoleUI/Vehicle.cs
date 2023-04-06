bd68b37aedde50b88f0e3a49cd21fe344194d939
adamacneill <adamichelee@gmail.com>
2023-04-05 20:38:39 -0700

    initial commit

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal abstract class Vehicle
+    {
+        public string Year { get; set; } = "default value";
+        public string Make { get; set; } = "default value";
+        public string Model { get; set; } = "default value";
+
+        public abstract void DriveAbstract();
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine("Default virtual drive method");
+        }
+    }
+}
